9883c1ed24346f62870beec803d7d728af8cecac
QuIC B <cquic.uofa@gmail.com>
2022-03-14 11:01:22 -0700

    fixed issue with unit norm error check

--- spin_utils.m
+++ spin_utils.m
@@ -206,7 +206,7 @@ function mustBeHalfInteger(J)
 end
 
 function mustBeUnitVector(n)
-    if ~(norm(n)==1)
+    if ~(abs(norm(n)-1)<(1.5*eps)) % n/norm(n) is good to +/- eps
         eidType = 'mustBeUnitVector:notUnitVector';
         msgType = 'Input must be unit vector.';
         throwAsCaller(MException(eidType,msgType))
